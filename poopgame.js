80daa82b64ad8ef1f031bdb3191cf6af2f53afa4
sara <saralundkvist86@gmail.com>
2020-08-01 12:01:19 +0200

    creating oncklick functions for all 3 doors

--- poopgame.js
+++ poopgame.js
@@ -1,5 +1,19 @@
 let doorImage1 = document.getElementById('door1');
+let doorImage2 = document.getElementById('door2');
+let doorImage3 = document.getElementById('door3');
+
 const poopPath="./poop.jpg";
+const goodPath1="./piccachou.jpg";
+const goodPath2="./love.jpg";
+
 doorImage1.onclick = () => {
     doorImage1.src = poopPath;
+}
+
+doorImage2.onclick = () => {
+    doorImage2.src = goodPath1;
+}
+
+doorImage3.onclick = () => {
+    doorImage3.src = goodPath2;
 }
