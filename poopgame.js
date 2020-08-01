6c970850744ae0758b00293f5c00c0a718a49a16
sara <saralundkvist86@gmail.com>
2020-08-01 12:24:18 +0200

    creates randomDoorGenerator() function to work together with onclick event

--- poopgame.js
+++ poopgame.js
@@ -6,14 +6,38 @@ const poopPath="./poop.jpg";
 const goodPath1="./piccachou.jpg";
 const goodPath2="./love.jpg";
 
+const numClosedDoors = 3;
+let openDoor1;
+let openDoor2;
+let openDoor3;
+
+const randomDoorGenerator = () => {
+    const poopDoor = Math.floor(Math.random() * numClosedDoors);
+    if (poopDoor === 0) {
+        openDoor1 = poopPath;
+        openDoor2 = goodPath1;
+        openDoor3 = goodPath2;
+    } else if (poopDoor === 1) {
+        openDoor1 = goodPath1;
+        openDoor2 = poopPath;
+        openDoor3 = goodPath2;
+    } else { (poopDoor === 2)
+        openDoor1 = goodPath2;
+        openDoor2 = goodPath1;
+        openDoor3 = poopPath;
+    }
+}
+
 doorImage1.onclick = () => {
-    doorImage1.src = poopPath;
+    doorImage1.src = openDoor1;
 }
 
 doorImage2.onclick = () => {
-    doorImage2.src = goodPath1;
+    doorImage2.src = openDoor2;
 }
 
 doorImage3.onclick = () => {
-    doorImage3.src = goodPath2;
+    doorImage3.src = openDoor3;
 }
+
+randomDoorGenerator();
